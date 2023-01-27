231d1e205945f7cfc8b6e28d6cb10b7f111c3103
gorobchenko-k <gorobchenko.es@mail.ru>
2023-01-27 16:14:37 +0300

    feat: add sort by columns

--- scripts/download-data.js
+++ scripts/download-data.js
@@ -1,16 +1,19 @@
 const tableBody = document.querySelector(".table__body");
 
-let strTable = '';
+function downloadData(people) {
+    let strTable = '';
 
-for (let index = 0; index < people.length; index++) {
-    const person = people[index];
-    strTable += `<tr>
+    for (let index = 0; index < people.length; index++) {
+        const person = people[index];
+        strTable += `<tr>
                     <td class="firstName">${person.name.firstName}</td>
                     <td class="lastName">${person.name.lastName}</td>
                     <td class="about"><p> ${person.about}</p></td>
                     <td class="eyeColor" style="background-color: ${person.eyeColor};">${person.eyeColor}</td>
                 </tr>`;
+    }
+
+    return strTable;
 }
 
-
-tableBody.innerHTML = strTable; // загружаем строки таблицы в <tbody>
+tableBody.innerHTML = downloadData(people); // загрузка строки таблицы в <tbody>
